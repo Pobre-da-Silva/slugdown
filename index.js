db1d6ed48c9f88166b0f4ecf44ad6725035c2811
decrovert <decrovertp@gmail.com>
2022-08-02 15:27:48 +0100

    making the library

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-/*test*/
+export const slugdown = {
+	slugdown_time: 200,
+
+	sleep: function(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	},
+
+	set_slugdown_time: function(time) {
+		this.slugdown_time = time;
+	},
+
+	get_slugdown_time: function(time) {
+		return this.slugdown_time;
+	},
+
+	slugdown: async function(target_func) {
+		await this.sleep(this.slugdown_time);
+		target_func();
+	}
+};
